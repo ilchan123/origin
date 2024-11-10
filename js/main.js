c5807b1bebb3a3eedd39b7dda4efd03bdc3aaeb1
ilchan123 <dlfcks9@gmail.com>
2024-11-11 01:01:42 +0900

    mouse

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@ document.addEventListener('wheel', (event) => {
         }
     }
 });
-
+ 
 // Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
 navbarMenu.addEventListener('click',()=>{
     navbarMenu.classList.toggle('close')
